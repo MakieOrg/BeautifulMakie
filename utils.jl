f5d07f7a69a5d822074d6aef00fecfba91b22637
lazarusA <lazarus.alon@gmail.com>
2021-06-07 12:44:56 +0200

    testing files

---
+++ utils.jl
@@ -0,0 +1,16 @@
+function hfun_bar(vname)
+  val = Meta.parse(vname[1])
+  return round(sqrt(val), digits=2)
+end
+
+function hfun_m1fill(vname)
+  var = vname[1]
+  return pagevar("index", var)
+end
+
+function lx_baz(com, _)
+  # keep this first line
+  brace_content = Franklin.content(com.braces[1]) # input string
+  # do whatever you want here
+  return uppercase(brace_content)
+end
