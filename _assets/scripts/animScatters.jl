1602a3eb358497036fbe9f10bf4ca182fc703f50
lazarusA <lazarus.alon@gmail.com>
2021-06-28 10:59:18 +0200

    added basic animations, scatters and lines
    Former-commit-id: da4003cd3626c47ff1d6f13bf7cbfb3407c9cd84

---
+++ _assets/scripts/animScatters.jl
@@ -0,0 +1,29 @@
+#by Lazaro Alonso
+using CairoMakie, Random
+CairoMakie.activate!()
+let
+    Random.seed!(123)
+    npts = 100
+    initms = 8*rand(npts)
+    msize = Node(initms) # this is variable that will change
+    with_theme(theme_dark()) do
+        # first frame, initial plot
+        fig, ax = scatter(2*rand(npts), rand(npts), markersize = msize,
+            color = initms, colormap = (:viridis, 0.75), strokewidth = 0.5,
+            strokecolor = :white, figure = (resolution=(700,450),),
+            axis = (xlabel = "x", ylabel = "y",))
+        limits!(ax, 0,2,0,1)
+        # the animation is done by updating the node values
+        record(fig,joinpath(@__DIR__, "output", "animScatters.mp4"), framerate = 24) do io
+            for i in 1:0.1:8
+                msize[] = i*initms
+                recordframe!(io)  # record a new frame
+            end
+        end
+    end
+end
+
+
+
+using Pkg # HIDE
+Pkg.status("CairoMakie") # HIDE
---
+++ animations/animLinePoint.md
@@ -0,0 +1,11 @@
+@def title = "animLinePoint"
+@def hascode = true
+@def tags = ["syntax", "code"]
+
+## Animation Line and Point
+\fig{/_assets/scripts/output/animLinePoint.mp4}
+\input{julia}{/_assets/scripts/animLinePoint.jl}
+~~~
+<span style="color:#e53e00;"> <strong> Dependencies </strong> </span>
+~~~
+\prettyshow{/_assets/scripts/animLinePoint}
