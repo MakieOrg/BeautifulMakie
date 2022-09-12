b9ad6054df657876afca2a0d4f97e22a5fad304c
Lazaro Alonso <lazarus.alon@gmail.com>
2022-09-12 20:00:46 +0200

    Update make.jl
    up make.jl file

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,7 @@
 using Documenter, DocumenterMarkdown, Pkg
 
 deployconfig = Documenter.auto_detect_deploy_system()
-Documenter.post_status(deployconfig; type="pending", repo="github.com/lazarusA/BeautifulMakie.git")
+Documenter.post_status(deployconfig; type="pending", repo="github.com/MakieOrg/BeautifulMakie.git")
 
 makedocs(; sitename="BeautifulMakie", authors="Lazaro", clean=true, doctest=true,
         strict=[
@@ -16,7 +16,7 @@ makedocs(; sitename="BeautifulMakie", authors="Lazaro", clean=true, doctest=true
          checkdocs=:all, format=Markdown(), draft=false,
          build=joinpath(@__DIR__, "docs"))
 
-deploydocs(; repo="github.com/lazarusA/BeautifulMakie.git", push_preview=true,
+deploydocs(; repo="github.com/MakieOrg/BeautifulMakie.git", push_preview=true,
            deps=Deps.pip("mkdocs", "pygments", "python-markdown-math", "mkdocs-material",
                          "pymdown-extensions", "mkdocstrings", "mknotebooks",
                          "pytkdocs_tweaks", "mkdocs_include_exclude_files", "jinja2",
