970ad0c366cc3646c049ef4fe6919294af616fd3
lazarusA <lazarus.alon@gmail.com>
2022-10-03 11:47:03 +0200

    how to cube

---
+++ examples/datavis/strange_attractors.jl
@@ -0,0 +1,80 @@
+# Here, we reproduce the outputs from this link: https://examples.pyviz.org/attractors/attractors.html
+
+# ## Strange attractors
+# An attractor is a set of values to which a numerical system tends to evolve. 
+# An attractor is called a strange attractor if the resulting pattern has a fractal structure.
+
+# In order to achieve the desire output we just need the following functions.
+using GLMakie, StatsBase, OnlineStats
+using LinearAlgebra, Interpolations
+# ## Trajectory
+
+function trajectory(fn, x0, y0, kargs; n = 1000) #  kargs = a, b, c, d
+    x, y = zeros(n+1), zeros(n+1)
+    x[1], y[1] = x0, y0
+    for i = 1:n
+        x[i+1], y[i+1] = fn(x[i], y[i], kargs...)
+    end
+    x, y
+end
+
+# ## Aggregation
+
+function aggHist(x, y; nbinsx = 400, nbinsy = 400)
+    xedges = range(extrema(x)..., length = nbinsx)
+    yedges = range(extrema(y)..., length = nbinsy)
+    o = fit!(HeatMap(xedges, yedges), zip(x, y))
+    return o
+end
+
+# ## Equalization
+
+function eq_hist(matrix; nbins = 256 * 256)
+    h_eq = fit(Histogram, vec(matrix), nbins = nbins)
+    h_eq = normalize(h_eq, mode = :density)
+    cdf = cumsum(h_eq.weights)
+    cdf = cdf / cdf[end]
+    edg = h_eq.edges[1]
+    interp_linear = LinearInterpolation(edg, [cdf..., cdf[end]])
+    out = reshape(interp_linear(vec(matrix)), size(matrix))
+    return out
+end
+
+# ## Clifford
+
+function Clifford(x, y, a, b, c, d)
+    sin(a * y) + c * cos(a * x), sin(b * x) + d * cos(b * y)
+end
+
+# Some interesting values
+
+cargs = [[0, 0, -1.3, -1.3, -1.8, -1.9],
+    [0, 0, -1.4, 1.6, 1.0, 0.7],
+    [0, 0, 1.7, 1.7, 0.6, 1.2],
+    [0, 0, 1.7, 0.7, 1.4, 2.0],
+    [0, 0, -1.7, 1.8, -1.9, -0.4],
+    [0, 0, 1.1, -1.32, -1.03, 1.54],
+    [0, 0, 0.77, 1.99, -1.31, -1.45],
+    [0, 0, -1.9, -1.9, -1.9, -1.0],
+    [0, 0, 0.75, 1.34, -1.93, 1.0],
+    [0, 0, -1.32, -1.65, 0.74, 1.81],
+    [0, 0, -1.6, 1.6, 0.7, -1.0],
+    [0, 0, -1.7, 1.5, -0.5, 0.7]
+]
+
+with_theme(theme_black()) do
+    fig = Figure(resolution = (1200,900))
+    axs = [Axis(fig[i,j], aspect=1) for i in 1:3 for j in 1:4]
+    for i in 1:12
+        x, y = trajectory(Clifford, cargs[i][1:2]..., cargs[i][3:end]; n = 1_000_000)
+        o = aggHist(x, y; nbinsx = 300, nbinsy = 300)
+        m = eq_hist(o.counts)
+        heatmap!(axs[i], o.xedges, o.yedges, m; colormap = :inferno)
+        axs[i].title = join(string.(cargs[1]), ", ")
+    end
+    hidedecorations!.(axs)
+    hidespines!.(axs)
+    rowgap!(fig.layout,5)
+    colgap!(fig.layout,1)
+    fig
+end
---
+++ examples/meshes/how_to_cube.jl
@@ -0,0 +1,94 @@
+using GLMakie, TestImages, FileIO
+using GeometryBasics, Colors
+using Downloads
+GLMakie.activate!()
+
+# ## Simple cubed mesh
+mr = Rect3f(Vec3f(-0.5), Vec3f(1))
+
+fig, ax, obj = mesh(mr; color = :white, transparency=true,
+    figure = (; resolution = (1200,600)))
+wireframe!(ax, mr; color = :black, transparency=true)
+mesh(fig[1,2], mr; color = [v[3] for v in coordinates(mr)],
+    colormap = :Spectral_11)
+fig
+
+# Adding some colours at random
+
+fig, ax, obj = mesh(mr; color = rand(length(coordinates(mr))),
+    colormap = :sunset, figure = (; resolution = (1200,600)))
+mesh(fig[1,2], mr; color = 1:length(coordinates(mr)),
+    colormap = :sunset)
+fig
+
+# ## A matrix (image) as colors
+
+img = testimage("chelsea")
+fig, ax, obj = mesh(mr; color =rand(10,10), interpolate=false,
+    colormap = :seaborn_icefire_gradient,
+    figure = (; resolution = (1200,600)))
+mesh(fig[1,2], mr; color = img, interpolate=false)
+fig
+
+# Note that the image is simple wrap around the mesh, and not in a goog way.
+
+# ## Wrap individual colors around mesh
+# Solution by ffreyer, define new uvs
+
+function meshcube(o=Vec3f(0), sizexyz = Vec3f(1))
+    uvs = map(v -> v ./ (3, 2), Vec2f[
+    (0, 0), (0, 1), (1, 1), (1, 0),
+    (1, 0), (1, 1), (2, 1), (2, 0),
+    (2, 0), (2, 1), (3, 1), (3, 0),
+    (0, 1), (0, 2), (1, 2), (1, 1),
+    (1, 1), (1, 2), (2, 2), (2, 1),
+    (2, 1), (2, 2), (3, 2), (3, 1),
+    ])
+    m = normal_mesh(Rect3f(Vec3f(-0.5) .+ o, sizexyz))
+    m = GeometryBasics.Mesh(meta(coordinates(m);
+        uv = uvs, normals = normals(m)), faces(m))
+end
+m = meshcube();
+
+## +z, +x, +y,
+## -x, -y, -z
+img = rand(RGBf, 2, 3)
+fig, ax, obj = mesh(m; color = img, interpolate=false,
+    figure = (; resolution = (1200,600)))
+mesh(fig[1,2], m; color = img)
+fig
+
+# Increasing the number of colours
+
+img = rand(RGBf, 2*6, 3*6)
+mesh(m; color = img, interpolate=false)
+
+# Putting an image
+
+img = testimage("chelsea");
+mesh(m; color = img, interpolate=false)
+
+# Now the image is splitted all around, hence, something else will be needed.
+# I found the following work around. Where, an image with ratio (2,3) is pre-process, 
+# and where each entry corresponds to a different face. 
+
+# Remember again, the position for each one of them are: 
+# +z, +x, +y,
+# -x, -y, -z
+
+# ## Individual images per face
+
+timgs = ["bark_512", "bark_he_512", "brick_wall_he_512",
+    "woolen_cloth_he_512", "wood_grain_he_512", "straw_he_512"];
+
+
+fig = Figure(figure_padding=0, resolution =(600,400))
+axs = [Axis(fig[i,j], aspect=1) for i in 1:2 for j in 1:3]
+[heatmap!(axs[i], testimage(timgs[i])) for i in 1:6]
+hidedecorations!.(axs)
+hidespines!.(axs)
+colgap!(fig.layout,0)
+rowgap!(fig.layout,0)
+imgOut = GLMakie.scene2image(fig.scene)
+
+mesh(m; color = imgOut, interpolate=false)
