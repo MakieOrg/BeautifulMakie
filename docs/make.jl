95017988c85148c6131491a7457dafbf9bae077d
Lazaro Alonso <lazarus.alon@gmail.com>
2024-01-01 23:26:56 +0100

    back back

---
+++ docs/make.jl
@@ -0,0 +1,13 @@
+using DocumenterVitepress # ] add https://github.com/LuxDL/DocumenterVitepress.jl.git
+using Documenter
+include("theme_light_dark.jl")
+set_theme!(merge(theme_latexfonts(), theme_light_dark()))
+
+# ENV["RASTERDATASOURCES_PATH"] = "/Users/lalonso/data/"
+makedocs(; sitename="BeautifulMakie", authors="Lazaro Alonso",
+    clean=true,
+    checkdocs=:all,
+    format=DocumenterVitepress.MarkdownVitepress(),
+    draft=false,
+    source="src", build=joinpath(@__DIR__, "docs_site/")
+    )
