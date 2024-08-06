f6dfcfd0137fbbce68277f64e0280b85cb79e4f8
Lazaro Alonso <lazarus.alon@gmail.com>
2024-08-06 20:03:14 +0200

    regression Mesh

--- examples/3d/meshes/isosurfaces.jl
+++ examples/3d/meshes/isosurfaces.jl
@@ -9,13 +9,14 @@ using Meshing, GeometryBasics
 GLMakie.activate!()
 
 isoval = 100
-algo = MarchingCubes(iso=isoval, insidepositive=false)
+algo = MarchingCubes(; iso=isoval)
 
 function show_isosurface(f,h,ξ; color=(:dodgerblue,0.5))
   s = [h(x,y,z) for x in ξ, y in ξ, z in ξ] .+ isoval
-  mc = GeometryBasics.Mesh(s, algo)
-
-  return mesh(f, normal_mesh(mc); color,
+  # generate the mesh using marching cubes
+  vts, fcs = isosurface(s, algo)
+  # mc = GeometryBasics.Mesh(s, algo)
+  return mesh(f, vts, map(v -> GeometryBasics.TriangleFace(v...), fcs); color,
       diffuse = Vec3f0(0.8),
       specular = Vec3f0(1.1),
       shininess = 30f0,
