39f2e75c576e008afb03c0880813bda29f0b50ef
Lazaro Alonso <lazarus.alon@gmail.com>
2025-11-19 21:23:46 +0100

    comments

--- examples/dashboards/matcap.jl
+++ examples/dashboards/matcap.jl
@@ -14,7 +14,7 @@ pyr = Pyramid(Point3f(0), 1.0f0, 1.0f0)
 rectmesh = Rect3(Point3f(-0.5), Vec3f(1))
 sphere = Sphere(Point3f(-0.5), 1)
 function Kone(; quality = 10)
-    # Create base circle points
+    ## Create base circle points
     base_radius = 0.5f0
     base_points = Point3f[]
     for i in 0:quality-1
@@ -24,26 +24,27 @@ function Kone(; quality = 10)
         push!(base_points, Point3f(x, y, 0))
     end
     
-    # Apex point
+    ## Apex point
     apex = Point3f(0, 0, 1)
     
-    # Create faces connecting base to apex
+    ## Create faces connecting base to apex
     faces = TriangleFace{Int}[]
     for i in 1:quality
         next_i = (i % quality) + 1
-        # Triangle: base point i, base point next, apex
+        ## Triangle: base point i, base point next, apex
         push!(faces, TriangleFace(i, next_i, quality + 1))
     end
-    # Base circle face
+    ## Base circle face
     for i in 2:quality-1
         push!(faces, TriangleFace(1, i, i + 1))
     end
     
-    # Combine all points
+    ## Combine all points
     all_points = vcat(base_points, [apex])
     
     return GeometryBasics.Mesh(all_points, faces)
 end
+
 cone = Kone()
 brain = load(Makie.assetpath("brain.stl"))
 matball = load(Makie.assetpath("matball_base.obj"))
